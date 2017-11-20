2e453ae9d797b52a509c57c0f4498e476af25d42
Reinhard Langner <Langner.R@eplan.de>
2017-11-20 19:35:05 +0100

    change on work NB

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>React Games</h1>
+        <h1>React Games !</h1>
         <div className="center">
           { /* <GameOfLife /> */ }
         </div>
