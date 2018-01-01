b4e39bf6a135297a1267d260136634a633e65f68
rene_imac <HarryMillerMail@googlemail.com>
2018-01-01 21:21:56 +0100

    first try canvas doodle

--- src/games/doodlejump/Doodle.js
+++ src/games/doodlejump/Doodle.js
@@ -1,11 +1,53 @@
 
-import React from 'react'
 
-const Doodle = (props) => {
-  const r = 20
-  return (
-    <circle className="doodle" cx={props.x-r} cy={props.y-r} r={r} />
-  )
+const KEY_RIGHT = 39
+const KEY_LEFT = 37
+
+
+class Doodle {
+
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+    this.dx = 5
+    this.radius = 20
+    this.g = 3
+    this.v = 0
+  }
+
+  update(state) {
+    const t = 0.3
+    this.v = this.v + t * this.g
+    this.y += t * this.v
+
+    if (this.y > state.height - 40) {
+      // bounce back
+      this.v = -this.v
+    }
+    if (state.keyValue === true) {
+      switch (state.keyCode) {
+        case KEY_RIGHT:
+          this.x += this.dx
+          break
+        case KEY_LEFT:
+          this.x -= this.dx
+          break
+        default:
+      }
+    }
+    if (this.y > state.height) {
+      state.stopGame = true
+    }
+  }
+
+
+  render(state) {
+    const context = state.context
+    context.beginPath();
+    context.fillStyle = '#723'
+    context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    context.fill();
+  }
 }
 
 export default Doodle
--- src/games/doodlejump/DoodleJump.js
+++ src/games/doodlejump/DoodleJump.js
@@ -11,60 +11,108 @@ class DoodleJump extends Component {
     super(props)
     const height = 600
     const width = 400
-    const stairs = []
-    for (let y = 40; y <= height; y+=70) {
-      stairs.push(
-        {
-          x: Math.random() * (width-70),
-          y
-        }
-      )
-    }
+
     this.state = {
       height,
       width,
-      stairs,
-      dx: width/2,
-      dy: height/2,
     }
 
-    this.update = this.update.bind(this)
+  /*
+  const stairs = []
+  for (let y = 40; y <= height; y+=70) {
+    stairs.push(
+      {
+        x: Math.random() * (width-70),
+        y
+      }
+    )
+  }
+  this.state = {
+    height,
+    width,
+    stairs,
+    dx: width/2,
+    dy: height/2,
+  }
 
-    this.interval = setInterval(this.update, 50)
+  this.update = this.update.bind(this)
+
+  this.interval = setInterval(this.update, 50)
+  */
   }
 
   componentWillMount() {
-    document.addEventListener('keydown', this.keyDown.bind(this), false)
+    document.addEventListener('keydown', this.keyHandler.bind(this, true))
+    document.addEventListener('keyup', this.keyHandler.bind(this, false))
   }
 
   componentWillUnmount() {
     clearInterval(this.interval);
-    document.removeEventListener('keydown', this.keyDown.bind(this), false)
-  }  
-
-  keyDown(e) {
-    this.lastKey = e.code
+    document.removeEventListener('keydown', this.keyHandler)
+    document.removeEventListener('keyup', this.keyHandler)
   }
 
-  componentDidMount() {}
-
-  update() {
+  keyHandler(value, event) {
     this.setState({
       ...this.state,
-      dy: this.state.dy+5
+      keyCode: event.keyCode,
+      keyValue: value
     })
   }
 
+  componentDidMount() {
+    const context = this.canvas.getContext('2d')
+    this.setState({
+      context
+    })
+
+    this.startGame()
+
+
+    requestAnimationFrame(() => {
+      this.update()
+    })
+  }
+
+  startGame() {
+    this.doodle = new Doodle(this.state.width / 2, this.state.height / 2)
+  }
+
+
+  update(timestamp) {
+    const context = this.state.context
+    context.save()
+
+    context.fillStyle = '#eee'
+    context.fillRect(0, 0, this.state.width, this.state.height)
+
+
+    this.doodle.update(this.state)
+
+    this.doodle.render(this.state)
+
+
+    context.restore()
+
+    if (this.state.stopGame) {
+      console.log("STOP")
+      return
+    }
+
+    requestAnimationFrame(() => {
+      this.update()
+    })
+
+  }
+
   render() {
+    const style = {
+      border: "1px solid black"
+    }
     return (
       <div>
         <p>use cursor left + right</p>
-        <svg width={this.state.width} height={this.state.height} >
-          { this.state.stairs.map((s,index) => {
-              return ( <Stair x={ s.x } y={ s.y } key={index} /> )
-            }) }
-          <Doodle x={this.state.dx} y={this.state.dy} />
-        </svg>
+        <canvas ref={ r => this.canvas = r } width={ this.state.width } height={ this.state.height } style={ style } />
       </div>
     )
   }
